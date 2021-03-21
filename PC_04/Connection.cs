64821fa8a4f6505a3a4f300f3db17dc003c00f14
Denisivansantoso982 <denisivansantoso@gmail.com>
2021-03-21 15:55:04 +0700

    Manage Connection

--- PC_04/Connection.cs
+++ PC_04/Connection.cs
@@ -8,6 +8,7 @@ namespace PC_04
 {
     class Connection
     {
-        public static string connectionString = @"Data Source=DENISIVANSANTOS;Initial Catalog=hotel_langit_7;Integrated Security=True";
+        //public static string connectionString = @"Data Source=DENISIVANSANTOS;Initial Catalog=hotel_langit_7;Integrated Security=True";
+        public static string connectionString = @"Data Source=LAPTOP-QK9HLMP4\HOYIRULSQL;Initial Catalog=hotel_langit_7;Integrated Security=True";
     }
 }
